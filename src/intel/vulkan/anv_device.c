dbba746a0c24b08695c9d3895f0c5cb20578b973
Jason Ekstrand <jason.ekstrand@intel.com>
2017-02-14 18:57:22 -0800

    anv: Implement VK_KHX_external_memory
    This is the trivial implementation that just exposes the extension
    string but exposes zero external handle types.
    
    Change-Id: I17a0d3e5cca7301df67d4e84a6980deb8411555b
    Reviewed-by: Chad Versace <chadversary@chromium.org>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -485,6 +485,10 @@ static const VkExtensionProperties device_extensions[] = {
       .specVersion = 1,
    },
    {
+      .extensionName = VK_KHX_EXTERNAL_MEMORY_EXTENSION_NAME,
+      .specVersion = 1,
+   },
+   {
       .extensionName = VK_KHX_EXTERNAL_SEMAPHORE_EXTENSION_NAME,
       .specVersion = 1,
    },
