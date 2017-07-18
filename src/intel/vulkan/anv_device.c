358deacef7b7092370576cb582303f71ef8580ed
Craig Stout <cstout@google.com>
2017-07-18 15:21:26 -0700

    Add VK_KHX_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME
    Change-Id: I609e45cd4e8216b8e3675a6c31b2a933498a9614

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -285,6 +285,10 @@ static const VkExtensionProperties device_extensions[] = {
       .extensionName = VK_KHX_EXTERNAL_SEMAPHORE_EXTENSION_NAME,
       .specVersion = 1,
    },
+   {
+      .extensionName = VK_KHX_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME,
+      .specVersion = 1,
+   },
 };
 
 static void *
